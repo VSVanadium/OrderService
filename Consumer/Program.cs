b2d8a55f1d503e48a2f55e2551da891cee79a280
VIKRANT SAXENA <vikrant.saxena@lhind.dlh.de>
2023-12-14 08:43:18 +0100

    Add data endpoint

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -6,13 +6,27 @@ Console.WriteLine("Hello, World!");
 using var channel = GrpcChannel.ForAddress("http://localhost:5039");
 var client = new Order.OrderClient(channel);
 
-Console.WriteLine(("Enter your name:"));
+Console.WriteLine(("product List:"));
+var productResponse = client.GetProducts(new ProductRequest { });
+
+List<Product> productList = productResponse.ProductList.ToList();
+
+foreach (var product in productList)
+{
+    Console.WriteLine(product.Id+" "+product.Name+":"+ product.Price+"$");
+}
+
+Console.WriteLine("");
+Console.WriteLine("Enter your name");
 var name = Console.ReadLine();
+
+Console.WriteLine(("Enter id of product you want to buy:"));
+var productId = Console.ReadLine();
 
 Console.WriteLine(("Enter product quantity:"));
 var quantity = Console.ReadLine();
 
-var request = new BillingRequest { Name = name, Product = "Bread", Quantity = quantity };
+var request = new BillingRequest { Name = name, ProductId = productId, Quantity = quantity };
 
 var response = client.Checkout(request);
 
